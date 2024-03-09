55326236cad057f38e4c132c90f2f01419136f77
lhaaaaa <lhanh@uneti.edu.vn>
2024-03-09 23:05:55 +0700

    thêm file

--- Lab 1/Code mẫu.py
+++ Lab 1/Code mẫu.py
@@ -1,44 +1,55 @@
+# Khai báo và gán giá trị cho biến b
 b = 6
-a_b_c = 7
-#print(type(b))
-a = 7.0
-#print(type(a))
-m ="Khoa học dữ liệu"
-#print(type(m))
-n = False
-#print(type(n))
 
-c = 10
+# Khai báo và gán giá trị cho biến a_b_c
+a_b_c = 7
+# In kiểu dữ liệu của biến b
+print(type(b))
+
+# Khai báo, gán giá trị cho biến a và in kiểu dữ liệu của biến a
+a = 7.0
+print(type(a))
+
+# Khai báo, gán giá trị cho biến m và in kiểu dữ liệu của biến m
+m ="Khoa học dữ liệu"
+print(type(m))
+
+# Khai báo, gán giá trị cho biến m và in kiểu dữ liệu của biến n
+n = False
+print(type(n))
+
+c = 10 
 d = 4
 print(c + d) #Đây là phép cộng số nguyên
-print(c - d)
-print(d - c)
-print(c * d)
-print(c/d)
-print(type(c/d))
-print(c%d)
-print(c//d)
-print(4**2)
-print(10**0.5)
-print(c > d)
-print(c == d)
-print(c > d and c == d)
-print(c > d or c == d)
+print(c - d) #Đây là phép trừ số nguyên
+print(d - c) 
+print(c * d) #Đây là phép nhân số nguyên
+print(c/d) #Đây là phép chia số nguyên
+print(type(c/d)) #Kiểm tra kiểu dữ liệu của phép chia số nguyên
+print(c%d) #Đây là phép chia lấy phần dư
+print(c//d) #Đây là phép chia lấy phần nguyên
+print(4**2) #Đây là phép lũy thừa
+print(10**0.5) # Lấy căn bậc 2, lũy thừa mũ 1/2
+print(c > d) #So sánh hai giá trị c và d, kết quả trả về giá trị Boolean
+print(c == d) 
+print(c > d and c == d) # Kết quả chỉ đúng khi các mệnh đề đều True
+print(c > d or c == d) # Kết quả đúng khi một trong các mệnh đề là True
 
-q = 5 + 4j
-print(type(q))
-print(q.real)
-print(q.imag)
+q = 5 + 4j #Khai báo số phức
+print(type(q)) 
+print(q.real) #Lấy phần thực
+print(q.imag) #Lấy phần ảo
 
-print('Đây là chuỗi ký tự')
+print('Đây là chuỗi ký tự') #In ra chuỗi ký tự sử dụng nháy đơn
 print("""Đây là chuỗi ký tự 2 """)
 
-#Kiểm tra số nguyên tố
+#Đây là chú thích 1 dòng
 
-#Đây là chú thích 1
-
-"""
+""" 
+Đây là chú thích nhiều dòng
 Đây là chú thích 1
 Đây là chú thích 2
 Đây là chú thích 3
 """
+
+
--- Lab 1/Nhập dữ liệu từ bàn phím.py
+++ Lab 1/Nhập dữ liệu từ bàn phím.py
@@ -8,7 +8,7 @@
 #Nhập nhiều dữ liệu từ bàn phím
 #a, b = input("Nhập dữ liệu từ bàn phím").split()
 
-#Ép nhiều kiểu dữ liệu nhập từ bàn phím
-a, b, c = map(float,input("Nhập các số thực từ bàn phím").split(",")) 
-print(type(a))
-print(type(b))
+#Ép kiểu dữ liệu vào nhiều biến nhập từ bàn phím
+#a, b, c = map(float,input("Nhập các số thực từ bàn phím").split(",")) 
+#print(type(a))
+#print(type(b))
